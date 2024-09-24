f5ea0628295eacad41dd5f459cdcd34c185ade0a
Juan Gaines <juan.gaines@platzi.com>
2024-09-24 09:32:14 -0500

    feat: libraries needed on toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.ksp) apply false
 }
