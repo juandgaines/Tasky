c8cc6f3ca3a8fb83b356ccfe0be7c4c71bc277c5
Juan Gaines <juan.gaines@platzi.com>
2024-09-30 07:01:45 -0500

    feat: splash screen

--- core/presentation/designsystem/src/main/java/com/juandgaines/core/presentation/designsystem/Colors.kt
+++ core/presentation/designsystem/src/main/java/com/juandgaines/core/presentation/designsystem/Colors.kt
@@ -2,19 +2,19 @@ package com.juandgaines.core.presentation.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-val TaskyBlack = Color(0xFF16161C) //ok
-val TaskyDarkGray = Color(0xFF5C5D5A)//Ok color text
-val TaskyGray= Color(0xFFA9B4BE)// ok  action text
+val TaskyBlack = Color(0xFF16161C)
+val TaskyDarkGray = Color(0xFF5C5D5A)
+val TaskyGray= Color(0xFFA9B4BE)
 val TaskyDarkGreen= Color(0xFF232624)
-val TaskyGreen= Color(0xFF279F70) //ok
-val TaskyLightGreen= Color(0xFFCAEF45) // ok
-val TaskyLightBlue= Color(0xFFB7C6DE)// ok for hint colors text
-val TaskyLight= Color(0xFFEEF6FF)// ok
-val TaskyLight2= Color(0xFFF2F3F7)// ok
-val TaskyWhite= Color(0xFFFFFFFF)//ok
-val TaskyBrown= Color(0xFF40492D) // ok  dots menu in event
-val TaskyOrange= Color(0xFFFDEFA8) //ok
-val TaksyRed= Color(0xFFFF7272) // ok
+val TaskyGreen= Color(0xFF279F70)
+val TaskyLightGreen= Color(0xFFCAEF45)
+val TaskyLightBlue= Color(0xFFB7C6DE)
+val TaskyLight= Color(0xFFEEF6FF)
+val TaskyLight2= Color(0xFFF2F3F7)
+val TaskyWhite= Color(0xFFFFFFFF)
+val TaskyBrown= Color(0xFF40492D)
+val TaskyOrange= Color(0xFFFDEFA8)
+val TaksyRed= Color(0xFFFF7272)
 
-val TaskyGrayHint = Color(0xFFA1A4B2) // ok hint text
+val TaskyGrayHint = Color(0xFFA1A4B2)
 
