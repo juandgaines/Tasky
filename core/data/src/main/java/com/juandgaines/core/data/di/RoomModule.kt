8d5e5f0607579665c27681aeeab9a33992735437
Juan Gaines <juan.gaines@platzi.com>
2024-09-30 21:38:49 -0500

    feat: db init

--- core/data/src/main/java/com/juandgaines/core/data/di/RoomModule.kt
+++ core/data/src/main/java/com/juandgaines/core/data/di/RoomModule.kt
@@ -3,10 +3,15 @@ package com.juandgaines.core.data.di
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 class RoomModule {
 
     @Provides
