5134f375e1c10d4066b1e2db35609096a9361f17
Juan Gaines <juan.gaines@platzi.com>
2024-10-02 08:18:00 -0500

    feat: action button

--- core/presentation/designsystem/src/main/java/com/juandgaines/core/presentation/designsystem/components/TaskyActionButton.kt
+++ core/presentation/designsystem/src/main/java/com/juandgaines/core/presentation/designsystem/components/TaskyActionButton.kt
@@ -1,6 +1,5 @@
 package com.juandgaines.core.presentation.designsystem.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,7 +11,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,7 +19,6 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.juandgaines.core.presentation.designsystem.TaskyGray
 import com.juandgaines.core.presentation.designsystem.TaskyTheme
 
