dfe324a0f33a81439ad39df946d46515f50fb093
Juan Gaines <juan.gaines@platzi.com>
2024-10-02 07:58:45 -0500

    feat: components text

---
+++ core/presentation/designsystem/src/main/java/com/juandgaines/core/presentation/designsystem/Icons.kt
@@ -0,0 +1,17 @@
+package com.juandgaines.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+
+val CheckIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_check)
+
+val EyeClosedIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_eye_closed)
+
+val EyeOpenedIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_eye_opened)
