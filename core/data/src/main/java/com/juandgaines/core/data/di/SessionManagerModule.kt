320dd5b78d60b9fbc7ff1c0f4368de25c2c3efdc
Juan Gaines <juan.gaines@platzi.com>
2024-10-07 06:59:09 -0500

    feat: changed interceptor to an specific class

--- core/data/src/main/java/com/juandgaines/core/data/di/SessionManagerModule.kt
+++ core/data/src/main/java/com/juandgaines/core/data/di/SessionManagerModule.kt
@@ -3,11 +3,13 @@ package com.juandgaines.core.data.di
 import android.content.SharedPreferences
 import com.juandgaines.core.data.auth.SharedPreferencesSessionManager
 import com.juandgaines.core.data.auth.TokenApi
+import com.juandgaines.core.data.network.AuthInterceptor
 import com.juandgaines.core.domain.auth.SessionManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import javax.inject.Singleton
 
 @Module
@@ -25,4 +27,10 @@ class SessionManagerModule {
             tokenApi = tokenApi
         )
     }
+
+    @Provides
+    @Singleton
+    fun providesAuthInterceptor(
+        sessionManager: SessionManager
+    ): Interceptor = AuthInterceptor(sessionManager)
 }
--- core/data/src/main/java/com/juandgaines/core/data/network/AuthInterceptor.kt
+++ core/data/src/main/java/com/juandgaines/core/data/network/AuthInterceptor.kt
@@ -9,8 +9,9 @@ import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Interceptor.Chain
 import okhttp3.Response
+import javax.inject.Inject
 
-class AuthInterceptor(
+class AuthInterceptor @Inject constructor(
     private val sessionManager: SessionManager
 ): Interceptor {
     override fun intercept(chain: Chain): Response {
