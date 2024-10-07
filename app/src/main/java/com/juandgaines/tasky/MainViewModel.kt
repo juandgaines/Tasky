a6fb021a2147890c84ca39c5136e94ef080b0b70
Juan Gaines <juan.gaines@platzi.com>
2024-10-07 07:26:47 -0500

    feat: Main Viewmodel checking auth to define where to navigate

---
+++ app/src/main/java/com/juandgaines/tasky/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.juandgaines.tasky
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.juandgaines.core.domain.auth.SessionManager
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val sessionStorage: SessionManager
+):ViewModel() {
+
+    var state by mutableStateOf(MainState())
+        private set
+
+    init {
+        viewModelScope.launch {
+            state = state.copy(isCheckingAuth = true)
+            state = state.copy(
+                isLoggedIn = sessionStorage.get() != null
+            )
+            state = state.copy(isCheckingAuth = false)
+        }
+    }
+}
