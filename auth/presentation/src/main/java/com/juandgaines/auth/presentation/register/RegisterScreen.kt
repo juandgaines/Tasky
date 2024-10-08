66e95195ff74b8bc0924e5abc26c24db1fbbdedc
Juan Gaines <juan.gaines@platzi.com>
2024-10-08 07:43:22 -0500

    feat: navigation added

--- auth/presentation/src/main/java/com/juandgaines/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/juandgaines/auth/presentation/register/RegisterScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FabPosition.Companion
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -104,6 +106,7 @@ fun RegisterScreen(
                     .height(120.dp)
             )
         },
+        fabPosition = FabPosition.Start,
         floatingActionButton = {
             TaskyFAB(
                 icon = BackIcon,
