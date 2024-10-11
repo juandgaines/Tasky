6416427675738335db12e2a96f81528aac0ce38a
Juan Gaines <juan.gaines@platzi.com>
2024-10-10 20:29:07 -0500

    feat: domain models and mappers for task and reminder

---
+++ agenda/data/src/main/java/com/juandgaines/agenda/data/mappers/TaskMappers.kt
@@ -0,0 +1,42 @@
+package com.juandgaines.agenda.data.mappers
+
+import com.juandgaines.agenda.data.task.remote.TaskRequest
+import com.juandgaines.agenda.data.task.remote.TaskResponse
+import com.juandgaines.agenda.domain.task.Task
+import com.juandgaines.core.data.database.task.TaskEntity
+
+fun Task.toTaskRequest() = TaskRequest(
+    id = id,
+    title = title,
+    description = description,
+    time = time,
+    remindAt = remindAt,
+    isDone = isDone
+)
+
+fun TaskResponse.toTask() = Task(
+    id = id,
+    title = title,
+    description = description,
+    time = time,
+    remindAt = remindAt,
+    isDone = isDone
+)
+
+fun TaskEntity.toTask() = Task(
+    id = id,
+    title = title,
+    description = description,
+    time = time,
+    remindAt = remindAt,
+    isDone = isDone
+)
+
+fun Task.toTaskEntity() = TaskEntity(
+    id = id,
+    title = title,
+    description = description,
+    time = time,
+    remindAt = remindAt,
+    isDone = isDone
+)
