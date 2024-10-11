05da618dc5870579c90ae88523055bf792804211
Juan Gaines <juan.gaines@platzi.com>
2024-10-11 11:53:16 -0500

    feat: repositories

--- agenda/data/src/main/java/com/juandgaines/agenda/data/reminder/di/ReminderModule.kt
+++ agenda/data/src/main/java/com/juandgaines/agenda/data/reminder/di/ReminderModule.kt
@@ -1,10 +1,14 @@
 package com.juandgaines.agenda.data.reminder.di
 
+import com.juandgaines.agenda.data.reminder.DefaultReminderRepository
 import com.juandgaines.agenda.data.reminder.remote.ReminderApi
+import com.juandgaines.agenda.domain.reminder.ReminderRepository
+import com.juandgaines.core.data.database.reminder.ReminderDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -15,5 +19,15 @@ class ReminderModule {
     @Singleton
     fun provideReminderApi(retrofit: Retrofit): ReminderApi {
         return retrofit.create(ReminderApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideReminderRepository(
+        reminderDao: ReminderDao,
+        reminderApi: ReminderApi,
+        applicationScope: CoroutineScope
+    ): ReminderRepository {
+        return DefaultReminderRepository(reminderDao, reminderApi, applicationScope)
     }
 }
