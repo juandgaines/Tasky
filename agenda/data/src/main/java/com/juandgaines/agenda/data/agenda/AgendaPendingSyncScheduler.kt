9ebed0e6c7ed8c661cea7bf0144c1e4ed1dad742
Juan Gaines <juan.gaines@platzi.com>
2024-10-20 15:17:56 -0500

    feat: fetch agenda Worker

--- agenda/data/src/main/java/com/juandgaines/agenda/data/agenda/AgendaPendingSyncScheduler.kt
+++ agenda/data/src/main/java/com/juandgaines/agenda/data/agenda/AgendaPendingSyncScheduler.kt
@@ -5,9 +5,11 @@ import androidx.work.BackoffPolicy.EXPONENTIAL
 import androidx.work.Constraints
 import androidx.work.NetworkType.CONNECTED
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.await
 import com.juandgaines.agenda.data.agenda.workers.DeleteAgendaTaskWorker
+import com.juandgaines.agenda.data.agenda.workers.FetchAgendaWorker
 import com.juandgaines.agenda.domain.agenda.AgendaItem
 import com.juandgaines.agenda.domain.agenda.AgendaSyncOperations
 import com.juandgaines.agenda.domain.agenda.AgendaSyncOperations.CreateAgendaItem
@@ -21,10 +23,13 @@ import com.juandgaines.core.data.database.agenda.AgendaSyncDao
 import com.juandgaines.core.data.database.agenda.DeleteReminderSyncEntity
 import com.juandgaines.core.data.database.agenda.DeleteTaskSyncEntity
 import com.juandgaines.core.domain.auth.SessionManager
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
+import kotlin.time.toJavaDuration
 
 class AgendaPendingSyncScheduler (
     private val context: Context,
@@ -39,9 +44,43 @@ class AgendaPendingSyncScheduler (
         when (syncType) {
             is CreateAgendaItem -> TODO()
             is DeleteAgendaItem -> scheduleDeleteAgendaItem(syncType.agendaItem)
-            is FetchAgendas -> TODO()
+            is FetchAgendas -> scheduleFetchAgendas(syncType.interval)
             is UpdateAgendaItem -> TODO()
         }
+    }
+
+    private suspend fun scheduleFetchAgendas(interval: Duration){
+
+        val isSyncScheduled = withContext(Dispatchers.IO){
+            workManager.getWorkInfosByTag("sync_work")
+                .get()
+                .isNotEmpty()
+        }
+        if(isSyncScheduled){
+            return
+        }
+
+        val workRequest = PeriodicWorkRequestBuilder<FetchAgendaWorker>(
+            repeatInterval = interval.toJavaDuration(),
+        )
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(CONNECTED)
+                    .build()
+            )
+            .setBackoffCriteria(
+                backoffPolicy = EXPONENTIAL,
+                backoffDelay = 2000L,
+                timeUnit = TimeUnit.MILLISECONDS
+            )
+            .setInitialDelay(
+                duration = 30,
+                timeUnit = TimeUnit.MINUTES
+            )
+            .addTag("sync_work")
+            .build()
+
+        workManager.enqueue(workRequest).await()
     }
 
     private suspend fun scheduleDeleteAgendaItem(agendaItem: AgendaItem) {
