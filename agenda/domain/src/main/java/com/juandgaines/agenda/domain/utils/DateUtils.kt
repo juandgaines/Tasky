36e5c39239784cf4447a7494023a06d60f72e9b2
Juan Gaines <juan.gaines@platzi.com>
2024-10-29 08:56:15 -0500

    feat: controls done

--- agenda/domain/src/main/java/com/juandgaines/agenda/domain/utils/DateUtils.kt
+++ agenda/domain/src/main/java/com/juandgaines/agenda/domain/utils/DateUtils.kt
@@ -44,13 +44,6 @@ fun LocalDate.toFormattedDate(): String {
     }
 }
 
-//get hour from LocalDate to LocalDateTime
-fun LocalDate.toFormattedTime(): String {
-    DateTimeFormatter.ofPattern("HH:mm").let {
-        return this.format(it)
-    }
-}
-
 fun LocalDate.startOfDay(): Long {
     return this.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
 }
