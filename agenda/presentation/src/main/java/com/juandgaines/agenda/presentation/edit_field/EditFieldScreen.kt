4e7abc860e0c6ecb74573f2b763001aa93c8cbe0
Juan Gaines <juan.gaines@platzi.com>
2024-10-31 16:56:01 -0500

    feat: edit field

--- agenda/presentation/src/main/java/com/juandgaines/agenda/presentation/edit_field/EditFieldScreen.kt
+++ agenda/presentation/src/main/java/com/juandgaines/agenda/presentation/edit_field/EditFieldScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -35,7 +37,7 @@ fun EditFieldScreenRoot(
     onSave: (String,String) -> Unit,
     navigateBack: () -> Boolean,
 ) {
-    val state = viewModel.state
+    val state by  viewModel.state.collectAsState()
     val events = viewModel.events
     EditFieldScreen(
         state = state,
--- agenda/presentation/src/main/java/com/juandgaines/agenda/presentation/edit_field/EditFieldViewModel.kt
+++ agenda/presentation/src/main/java/com/juandgaines/agenda/presentation/edit_field/EditFieldViewModel.kt
@@ -6,12 +6,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import com.juandgaines.agenda.presentation.edit_field.EditFieldAction.Save
 import com.juandgaines.core.presentation.navigation.ScreenNav
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,17 +27,20 @@ class EditFieldViewModel @Inject constructor(
     private var eventChannel = Channel<EditFieldEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    var state by mutableStateOf(EditFieldState())
-        private set
-
+    private var _state  = MutableStateFlow(EditFieldState())
     private val editValueData =  savedStateHandle.toRoute<ScreenNav.EditField>()
 
-    init {
-        state = state.copy(
-            fieldName = editValueData.fieldName,
-            fieldValue = TextFieldState(editValueData.fieldValue)
+    var state = _state
+        .onStart {
+            _state.value = EditFieldState(
+                fieldName = editValueData.fieldName,
+                fieldValue = TextFieldState(editValueData.fieldValue))
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = EditFieldState()
         )
-    }
 
     fun onAction(action: EditFieldAction) {
         when (action) {
